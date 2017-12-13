28a258e281b09d4ba3a7c0b4b2f1d69a6598c879
achernyak <artemchernyak@gmail.com>
2017-12-13 07:37:24 -0600

    building server

---
+++ webpack.server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+module.exports = {
+    // Inform webpack that we're build a bundle
+    // for nodeJS, rather than for the browser
+    target: 'node',
+
+    // root file of our server application
+    entry: './src/index.js',
+
+    // where to put the output file that is generated
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+
+    // Rub babel on every file it runs through
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react',
+                        'stage-0',
+                        ['env', { targets: { browsers: ['last 2 versions'] }}]
+                    ]
+                }
+            }
+        ]
+    }
+
+};
